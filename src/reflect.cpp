078fa3fa1d028fd3022d090e6eb183b7d45d319c
Rémi Attab <remi.attab@gmail.com>
2014-03-26 11:28:02 -0400

    Unified the core into a single compilation unit.
    Probably a dumb idea.

---
+++ src/reflect.cpp
@@ -0,0 +1,13 @@
+/* reflect.cpp                                 -*- C++ -*-
+   Rémi Attab (remi.attab@gmail.com), 26 Mar 2014
+   FreeBSD-style copyright and disclaimer apply
+
+   Compilation unit for the reflect code.
+*/
+
+#include "reflect.h"
+
+#include "registry.cpp"
+#include "value.cpp"
+#include "function.cpp"
+#include "reflection.cpp"
