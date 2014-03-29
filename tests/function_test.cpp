bcbb64f026d408bddd95b404cfbb9d06e7bd55c9
Rémi Attab <remi.attab@gmail.com>
2014-03-28 20:33:23 -0400

    Added a truck-load of test for Function which don't currently pass.

--- tests/function_test.cpp
+++ tests/function_test.cpp
@@ -21,7 +21,7 @@ BOOST_AUTO_TEST_CASE(basics)
 {
     using reflect::reflect;
 
-    auto foo = [] (unsigned i, int j) -> unsigned {
+    auto foo = [] (unsigned i, int& j, const int&, int&&) -> unsigned {
         return i + 1 * j;
     };
     Function fn("foo", foo);
@@ -29,100 +29,37 @@ BOOST_AUTO_TEST_CASE(basics)
     BOOST_CHECK_EQUAL(fn.returnType().type(), reflect<unsigned>());
     BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::Value);
 
-    BOOST_CHECK_EQUAL(fn.arguments(), 2);
+    BOOST_CHECK_EQUAL(fn.arguments(), 4);
+
+    BOOST_CHECK(!fn.argument(0).isConst());
     BOOST_CHECK_EQUAL(fn.argument(0).type(), reflect<unsigned>());
     BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::Value);
+
+    BOOST_CHECK(!fn.argument(1).isConst());
     BOOST_CHECK_EQUAL(fn.argument(1).type(), reflect<int>());
-    BOOST_CHECK_EQUAL(fn.argument(1).refType(), RefType::Value);
+    BOOST_CHECK_EQUAL(fn.argument(1).refType(), RefType::LValue);
 
-    BOOST_CHECK( fn.test(fn));
-    BOOST_CHECK( fn.test<unsigned(unsigned, int)>());
-    BOOST_CHECK( fn.test<void    (unsigned, int)>());
-    BOOST_CHECK(!fn.test<unsigned(int, unsigned)>());
+    BOOST_CHECK(fn.argument(2).isConst());
+    BOOST_CHECK_EQUAL(fn.argument(2).type(), reflect<int>());
+    BOOST_CHECK_EQUAL(fn.argument(2).refType(), RefType::LValue);
 
-    BOOST_CHECK_EQUAL(fn.call<unsigned>(1u, 2), foo(1, 2));
-    fn.call<void>(1u, 2);
-}
-
-BOOST_AUTO_TEST_CASE(test)
-{
-    BOOST_CHECK(std::is_reference<typename GetReturnValue<int&()>::type>::value);
-
-    BOOST_CHECK_EQUAL(reflectReturn<int&()>().refType(), RefType::LValue);
-
-    {
-        Function fn("foo", [] (int) -> int { return 1; });
-
-        BOOST_CHECK(!fn.returnType().isConst());
-        BOOST_CHECK(!fn.argument(0).isConst());
-        BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::Value);
-        BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::Value);
-
-        BOOST_CHECK( fn.test<void(int)>());
-
-        BOOST_CHECK( fn.test<int(int)>());
-
-        BOOST_CHECK( fn.test<int(int&)>());
-        BOOST_CHECK(!fn.test<int&(int)>());
-        BOOST_CHECK( fn.test<int(const int&)>());
-        BOOST_CHECK( fn.test<const int&(int)>());
-
-        BOOST_CHECK( fn.test<int(int&&)>());
-    }
-
-    {
-        Function fn("foo", [] (int& i) -> int& { return i; });
-
-        BOOST_CHECK(!fn.returnType().isConst());
-        BOOST_CHECK(!fn.argument(0).isConst());
-        BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::LValue);
-        BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::LValue);
-
-        BOOST_CHECK( fn.test<void(int&)>());
-
-        BOOST_CHECK(!fn.test<int&(int)>());
-        BOOST_CHECK( fn.test<int(int&)>());
-
-        BOOST_CHECK( fn.test<int&(int&)>());
-        BOOST_CHECK(!fn.test<int&(const int&)>());
-        BOOST_CHECK( fn.test<const int&(int&)>());
-
-        BOOST_CHECK(!fn.test<int&(int&&)>());
-    }
-
-    {
-        Function fn("foo", [] (const int& i) -> const int& { return i; });
-
-        BOOST_CHECK(fn.returnType().isConst());
-        BOOST_CHECK(fn.argument(0).isConst());
-        BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::LValue);
-        BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::LValue);
-
-        BOOST_CHECK( fn.test<void(const int&)>());
-
-        BOOST_CHECK( fn.test<const int&(int)>());
-        BOOST_CHECK( fn.test<int(const int&)>());
-
-        BOOST_CHECK(!fn.test<int&(const int&)>());
-        BOOST_CHECK( fn.test<const int&(int&)>());
-        BOOST_CHECK( fn.test<const int&(const int&)>());
-
-        BOOST_CHECK( fn.test<const int&(int&&)>());
-    }
-
-    {
-        Function fn("foo", [] (int&&) {});
-
-        BOOST_CHECK(fn.returnType().isVoid());
-        BOOST_CHECK(!fn.argument(0).isConst());
-        BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::RValue);
+    BOOST_CHECK(!fn.argument(3).isConst());
+    BOOST_CHECK_EQUAL(fn.argument(3).type(), reflect<int>());
+    BOOST_CHECK_EQUAL(fn.argument(3).refType(), RefType::RValue);
 
 
-        BOOST_CHECK(!fn.test<void(int)>());
-        BOOST_CHECK(!fn.test<void(int&)>());
-        BOOST_CHECK( fn.test<void(int&&)>());
-        BOOST_CHECK(!fn.test<void(const int&)>());
-    }
+    BOOST_CHECK(fn.test(fn));
+
+    int i = 2;
+    const unsigned expected = foo(1, i, 3, std::move(i));
+
+    fn.call<void>(1u, i, 3, std::move(i));
+
+    BOOST_CHECK_EQUAL(fn.call<unsigned>(1u, i, 3, std::move(i)), expected);
+
+    Value ret = fn.call<Value>(1u, i, 3, std::move(i));
+    BOOST_CHECK_EQUAL(ret.cast<unsigned>(), expected);
+
 }
 
 BOOST_AUTO_TEST_CASE(voids)
@@ -139,49 +76,122 @@ BOOST_AUTO_TEST_CASE(voids)
     fn.call<void>();
 }
 
-BOOST_AUTO_TEST_CASE(lValue)
+BOOST_AUTO_TEST_CASE(value_test)
 {
-    using reflect::reflect;
+    Function fn("foo", [] (int i) -> int { return i; });
 
-    auto foo = [] (unsigned& i, unsigned j) -> unsigned& {
-        i += j;
+    BOOST_CHECK(!fn.returnType().isConst());
+    BOOST_CHECK(!fn.argument(0).isConst());
+    BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::Value);
+    BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::Value);
+
+    BOOST_CHECK( fn.test<void(int)>());
+
+    BOOST_CHECK( fn.test<int(int)>());
+
+    BOOST_CHECK( fn.test<int(int&)>());
+    BOOST_CHECK(!fn.test<int&(int)>());
+    BOOST_CHECK( fn.test<int(const int&)>());
+    BOOST_CHECK( fn.test<const int&(int)>());
+
+    BOOST_CHECK( fn.test<int(int&&)>());
+}
+
+BOOST_AUTO_TEST_CASE(value_call)
+{
+    auto foo = [] (int i) -> int { return i + 1; };
+    Function fn("foo", foo);
+
+    BOOST_CHECK_EQUAL(fn.call<int>(10), foo(10));
+
+    Value ret = fn.call<Value>(Value(10));
+    BOOST_CHECK_EQUAL(ret.cast<int>(), foo(10));
+
+    int i = 10;
+    BOOST_CHECK_EQUAL(fn.call<int>(Value(i)), foo(i));
+
+    const int c = 10;
+    BOOST_CHECK_EQUAL(fn.call<int>(Value(c)), foo(c));
+}
+
+BOOST_AUTO_TEST_CASE(lValue_test)
+{
+    Function fn("foo", [] (int& i) -> int& { return i; });
+
+    BOOST_CHECK(!fn.returnType().isConst());
+    BOOST_CHECK(!fn.argument(0).isConst());
+    BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::LValue);
+    BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::LValue);
+
+    BOOST_CHECK( fn.test<void(int&)>());
+
+    BOOST_CHECK(!fn.test<int&(int)>());
+    BOOST_CHECK( fn.test<int(int&)>());
+
+    BOOST_CHECK( fn.test<int&(int&)>());
+    BOOST_CHECK(!fn.test<int&(const int&)>());
+    BOOST_CHECK( fn.test<const int&(int&)>());
+
+    BOOST_CHECK(!fn.test<int&(int&&)>());
+}
+
+BOOST_AUTO_TEST_CASE(lValue_call)
+{
+    auto foo = [] (int& i) -> int& {
+        i += 1;
         return i;
     };
     Function fn("foo", foo);
 
+}
+
+BOOST_AUTO_TEST_CASE(constLValue_test)
+{
+    Function fn("foo", [] (const int& i) -> const int& { return i; });
+
+    BOOST_CHECK(fn.returnType().isConst());
+    BOOST_CHECK(fn.argument(0).isConst());
     BOOST_CHECK_EQUAL(fn.returnType().refType(), RefType::LValue);
     BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::LValue);
 
-    BOOST_CHECK( fn.test(fn));
-    BOOST_CHECK( fn.test<unsigned&(unsigned&,  unsigned )>());
-    BOOST_CHECK(!fn.test<unsigned&(unsigned,   unsigned )>());
-    BOOST_CHECK( fn.test<unsigned (unsigned&,  unsigned )>());
-    BOOST_CHECK( fn.test<unsigned&(unsigned&,  unsigned&)>());
-    BOOST_CHECK(!fn.test<unsigned&(unsigned&&, unsigned )>());
+    BOOST_CHECK( fn.test<void(const int&)>());
 
-    unsigned value = 0;
-    {
-        auto& ret = fn.call<unsigned&>(value, 10u);
-        BOOST_CHECK_EQUAL(value, 10);
-        BOOST_CHECK_EQUAL(&ret, &value);
-    }
+    BOOST_CHECK( fn.test<const int&(int)>());
+    BOOST_CHECK( fn.test<int(const int&)>());
 
-    {
-        unsigned arg = 10;
-        cerr << endl;
-        Value ret = fn.call<Value>(Value(value), arg);
-        BOOST_CHECK_EQUAL(value, 20);
-        BOOST_CHECK_EQUAL(&ret.cast<unsigned>(), &value);
-    }
+    BOOST_CHECK(!fn.test<int&(const int&)>());
+    BOOST_CHECK( fn.test<const int&(int&)>());
+    BOOST_CHECK( fn.test<const int&(const int&)>());
 
-    {
-        fn.call<void>(value, Value(10u));
-        BOOST_CHECK_EQUAL(value, 30);
-    }
+    BOOST_CHECK( fn.test<const int&(int&&)>());
+}
+
+BOOST_AUTO_TEST_CASE(constLValue_call)
+{
+    auto foo = [] (const int& i) -> const int& { return i; };
+    Function fn("foo", foo);
+
 }
 
 
-BOOST_AUTO_TEST_CASE(rValue)
+BOOST_AUTO_TEST_CASE(rValue_test)
 {
+    Function fn("foo", [] (int&&) {});
+
+    BOOST_CHECK(fn.returnType().isVoid());
+    BOOST_CHECK(!fn.argument(0).isConst());
+    BOOST_CHECK_EQUAL(fn.argument(0).refType(), RefType::RValue);
+
+
+    BOOST_CHECK( fn.test<void(int)>());
+    BOOST_CHECK(!fn.test<void(int&)>());
+    BOOST_CHECK( fn.test<void(int&&)>());
+    BOOST_CHECK(!fn.test<void(const int&)>());
+}
+
+BOOST_AUTO_TEST_CASE(rValue_call)
+{
+    auto foo = [] (int&& i) { return i + 1; };
+    Function fn("foo", foo);
 
 }
